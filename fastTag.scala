982c78f52764221b1d3efb04e978caaf36c0df8e
kaviya <55015958+kaviya15@users.noreply.github.com>
2021-07-19 00:02:22 +0530

    Update fastTag.scala

--- fastTag.scala
+++ fastTag.scala
@@ -10,12 +10,20 @@ class getFastTag(var registerdUsers:HashMap[String,HashMap[String,String]]){
         println("Enter Owner Name ")
         var name = scala.io.StdIn.readLine();
         //GET VEHICLE TYPE
-        println("Enter vehicle type")
-        var vehicleModel = scala.io.StdIn.readLine()
+        println("Enter vehicle type's Index")
+        var vehicleModel= listModel
         // appending data to Map
-        userdetails+= (("vehicleModel" ,vehicleModel),("name" ,name),("checkedInTime",new Date().getTime().toString()))
+        userdetails+= (("vehicleModel" ,vehicleModel), ("modelPrice",findModel(vehicleModel)) ,("name" ,name),("checkedInTime",new Date().getTime().toString()))
         return(userdetails)
   }
+   private def listModel:String={
+       var models = Array("car/jeep/van","bus/truck","1-3axlevehicle","4-6axlevehicle","HCM/EME ","7-moreaxlevehicle")
+       for(model <- 0  until models.length){
+           println(s"${model}. ${models(model)}")
+       }
+      var vehicleModel = scala.io.StdIn.readInt()
+      return(models(vehicleModel))
+   }
 
     // register the vehicle 
     private def registerVehicle(vehicleNumber:String,vehicleDetails:HashMap[String,String]){
@@ -23,50 +31,42 @@ class getFastTag(var registerdUsers:HashMap[String,HashMap[String,String]]){
     }
     
     // Get fare for particular Type 
-    private def findModel(vehicleModel:String):Int={
-         var vehModel = vehicleModel.toLowerCase()
-         var carTypes:Array[String] = Array("car","jeep","van")
-         var busTypes:Array[String] = Array("bus","truck")
-         var axleTypes:Array[String] = Array("1-axlevehicle","2-axlevehicle","3-axlevehicle")
-         var highAxleTypes:Array[String] = Array("4-axlevehicle","5-axlevehicle","6-axlevehicle","hcm","eme")
-         if(vehicleModel.toLowerCase() == "lcv") return(135)
-         else if(carTypes.indexOf(vehModel)>=0)return(85)
-         else if (busTypes.indexOf(vehModel)>=0) return(285)
-         else if (axleTypes.indexOf(vehModel)>=0)return(315)
-         else if (highAxleTypes.indexOf(vehModel)>=0)return(450)    
-         else if (vehModel.split('-')(1) == "axlevehicle")return(550)
-         else return(0)
+    private def findModel(vehicleModel:String):String={
+        var vehModel = Map("car/jeep/van" -> 85 ,
+        "bus/truck"-> 285,"1-3axlevehicle"-> 315,"4-6axlevehicle"->450,"HCM/EME "->450 , "7-moreaxlevehicle"-> 550)
+        return(vehModel.get(vehicleModel).get.toString())
     }
 
     // calculate the total bill amount
       def billFare(vehicleNumber:String,registered:Boolean){ 
         if(registered) {
                   var getVehicleData  = registerdUsers.get(vehicleNumber)
-                  var billAmountForModel:Int = findModel(getVehicleData.get("vehicleModel"))
+                  var billAmountForModel = getVehicleData.get("modelPrice").toInt
                   var timeSeconds = getVehicleData.get("checkedInTime").toLong/60000
                   lazy val time = new Date().getTime()/60000
                   if((timeSeconds-time)<=30)
                         println(s"Total bill = ${billAmountForModel/2}")
                   else
                         println(s"Total bill = ${billAmountForModel}")
-              } 
+        }
 
         else{
              // Get Details from user 
             var vehicleData:HashMap[String,String] =  getDetailsFromUser(vehicleNumber)
-            var billAmountForModel = findModel(vehicleData.get("vehicleModel").get)
+            var billAmountForModel = vehicleData.get("modelPrice").get.toInt
             println(s"Total bill = ${billAmountForModel+50} // current toll fare ${billAmountForModel} + 50 registration fee")
             // //register account
-            registerVehicle(vehicleNumber,vehicleData)  
+            registerVehicle(vehicleNumber,vehicleData)
         }
     }
     
 
-    
 }
 
-object fastTag extends App{
 
+
+object fastTag extends App{
+    
     var registerdUsers:HashMap[String,HashMap[String,String]] =  HashMap()
     val fastTagUser:getFastTag = new getFastTag(registerdUsers)
     
@@ -77,15 +77,18 @@ object fastTag extends App{
     //FETCH THE USER DETAILS IF USER EXISTS
     if(registerdUsers.contains(vehicleNumber))
         fastTagUser.billFare(vehicleNumber,true)
+    
     else
     {
          val questions =  Array("Vehicle Number you entered doesn't have a fast tag",
         "Choose 1 to Register for fastTag","Choose 2 to exit","Enter your option: ")
          questions.foreach(arr=>println(arr))
-        
+
          val optionSelected:Int =  scala.io.StdIn.readInt();
-         if(optionSelected==1)
+
+         if(optionSelected==1){
                 fastTagUser.billFare(vehicleNumber , false)
-         
+         }
     }
 }
+
